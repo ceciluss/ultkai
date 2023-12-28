b2775cd71a004d2a63c028f778774a22921ec443
ceciluss <97836336+ceciluss@users.noreply.github.com>
2023-12-28 16:41:17 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,7 +1,6 @@
 branch=main
-git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
+git clone -b $branch https://github.com/ceciluss/Ultroid /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-pip3 uninstall telegraph && pip3 install telegraph
 docker run --privileged --env-file .env --rm -i ultroid 
